2d53049dad566157a38b42a6d0a377fc236dce51
leo <zhou6159@gmail.com>
2024-06-16 18:38:30 +0800

    Update settings.py
    code418处理，需要更新为实际的REQUEST_HEADERS

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -39,10 +39,10 @@ DOWNLOAD_DELAY = 5
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+}
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
